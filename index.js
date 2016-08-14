f282d59c032b5839ed4f262bc1d7f8992dd0ea7d
Ten <tenloh@gmail.com>
2016-08-13 23:40:54 -0400

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var models = require('./routes/monteDB.js');
 var User = models.User;
 var Game = models.Game;
+var swig = require('swig');
+var path = require('path');
 
 //SETTING UP ROUTING
 var app = express();
@@ -15,19 +17,60 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var gameId,
-    userId; 
+// point res.render to the proper directory
+app.set('views', __dirname + '/views');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files
+// have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn of swig's caching
+swig.setDefaults({cache: false});
+
+app.use(express.static(path.join(__dirname, '/views')));
+
+var game,
+    user,
+    carDoor,
+    firstGuess;
 
 app.get('/', function(req, res, next){
+  setUpGame();
   User.create({
     name: 'Ten Loh Test'
   })
-  .then(function(response){
-      res.send('Hello, ' + response.name + '.You have reached Monte Hall Web App!');
+  .then(function(userResponse){
+    user = userResponse;
+    return Game.create({
+      carDoor: 1
+    }).then(function(gameResponse){
+      game = gameResponse;
+      gameResponse.setPlayer(userResponse.id);
+    });
   })
-  .catch(next)
+  .then(function(response){
+      //res.send('Hello, ' + user.name + '.You have reached Monte Hall Web App!');
+      res.render('MonteHall');
+  })
+  .catch(next);
+
+  //res.send('Hello, you've reached the Monte Hall Web App!);
 
 });
+
+app.post('/guess', function(req, res, next){
+  var guess = req.body.guess;
+  game.firstGuess = guess;
+  if(guess === carDoor){
+    game.won = true;
+  }
+  game.save()
+  .then(function(response){
+    res.send('You have won!');
+  })
+  .catch(next);
+});
+
 
 app.get('/favicon.ico', function(req, res, next){
   next();
@@ -48,6 +91,13 @@ models.db.sync({force: true})
 .catch(function(err) { 
   console.error(err)
 })
+
+
+//Set up Game
+var setUpGame = function(){
+  carDoor = Math.ceil(Math.random() * 3);
+  firstGuess == null;
+}
 
 
 
--- routes/monteDB.js
+++ routes/monteDB.js
@@ -2,7 +2,9 @@
 
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://localhost:5432/MonteHallDB');
+var db = new Sequelize('postgres://localhost:5432/MonteHallDB', {
+  logging: false
+});
 
 
 
